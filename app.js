d130a58ac0fe8d080b18deaf0c42e49b3d97884d
Shofiq Saif <sislam161079@gmail.com>
2023-07-05 12:34:05 +0600

    First Commit

--- app.js
+++ app.js
@@ -1,14 +1,87 @@
-const express = require('express')
+const express = require("express");
 const app = express();
 const port = 3000;
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
+var https = require("https");
+var fs = require("fs");
 
-const urlencodedParser = bodyParser.urlencoded({extended:false});
+const urlencodedParser = bodyParser.urlencoded({ extended: true });
+app.use(express.static("public"));
 
-app.get("/",(req,res)=>{
-    res.sendFile(__dirname+"/signup.html");
+const mailchimp = require("@mailchimp/mailchimp_marketing");
+mailchimp.setConfig({
+    apiKey: "62a1591c0e61861a0ec4e8ce598aeb38-us9",
+    server: "us9",
 });
 
-app.listen(port,()=>{
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/signup.html");
+});
+
+app.get("/failure" , (req,res) => {
+    res.redirect("/");
+});
+
+app.post("/", urlencodedParser, (req, res) => {
+    console.log(req.body);
+    var firstName = req.body.fname;
+    var lastName = req.body.lname;
+    var email = req.body.email;
+
+    var data = {
+        email_address: email,
+        status: "subscribed",
+        merge_fields:{
+            FNAME : firstName,
+            LNAME : lastName,
+        }
+    };
+    // POST
+    var options = {
+        method: "POST",
+        hostname: "us9.api.mailchimp.com",
+        path: "/3.0/lists/a933c15de4/members?skip_merge_validation=false",
+        headers: {
+            Authorization: "us9 62a1591c0e61861a0ec4e8ce598aeb38-us9",
+            "Content-Type": "application/json",
+        },
+        maxRedirects: 20,
+    };
+
+    var REQ = https.request(options, function (RES) {
+        var chunks = [];
+
+        RES.on("data", function (chunk) {
+            chunks.push(chunk);
+            console.log(JSON.parse(chunk));
+            console.log("Status Code : " + RES.statusCode);
+
+            if(RES.statusCode == 200){
+                res.sendFile(__dirname + "/success.html");
+            }
+            else{
+                res.sendFile(__dirname + "/failure.html");
+            }
+        });
+        RES.on("error", function (error) {
+            console.error(error);
+        });
+    });
+
+    var postData = JSON.stringify(data);
+    console.log("THIS is postdata: " + postData);
+
+    REQ.write(postData);
+    REQ.end();
+
+    // MAILCHIMP API KEY
+    // 62a1591c0e61861a0ec4e8ce598aeb38-us9
+
+    // List Audiance Id
+    // a933c15de4
+});
+
+app.listen(port, () => {
     console.log("Server is running on port " + port);
 });
+
